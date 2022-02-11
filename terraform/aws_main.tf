4df234a8f637aea8c67c0f5bd8cbeb4af54df275
Devin Collins <me@imdevinc.com>
2022-02-11 12:48:50 -0800

    Cleanup

--- terraform/aws_main.tf
+++ terraform/aws_main.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "imdevinc-tf-storage"
-#     key    = "gold-rush"
-#     region = "us-west-1"
-#   }
-# }
-
 provider "aws" {
   region = "us-west-2"
 }
